b8bdf954d72fa33dbed7f06e2c1a36ee855bd3e9
김도언 <vov06144@naver.com>
2022-08-04 00:42:53 +0900

    Feat: 1959번 문제 미해결(Runtime Error)

--- NBA_doeon/week_1st/1959.py
+++ NBA_doeon/week_1st/1959.py
@@ -1,1 +1,18 @@
 # 두 개의 숫자열
+
+T = int(input())
+for test_case in range(1, T + 1):
+    
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    num = []
+    
+    for i in range((len(B) - len(A) + 1)):
+        sum = 0
+        for j in range(len(A)):
+            sum += (A[j] * B[i + j])
+        num.append(sum)
+        
+    answer = max(num)
+    print(f'#{test_case} {answer}')
