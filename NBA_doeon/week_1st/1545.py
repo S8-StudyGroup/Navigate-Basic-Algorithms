43b625c1f9d6bc742a7b1f4a94d5714b511b31c8
김도언 <vov06144@naver.com>
2022-08-03 23:20:24 +0900

    Feat: 1545번 문제 해결

--- NBA_doeon/week_1st/1545.py
+++ NBA_doeon/week_1st/1545.py
@@ -1,1 +1,9 @@
 # 거꾸로 출력해 보아요
+
+# range의 숫자를 역순으로 생성하려면 증가 폭을 음수로 지정
+# ex. range(10, 0, -1)는 10부터 1까지 -1씩 증가하는 숫자를 생성
+
+num = int(input())
+
+for number in range(num, -1, -1): ## num에서 0까지 -1씩 줄어드는 range
+    print(number, end=' ')
