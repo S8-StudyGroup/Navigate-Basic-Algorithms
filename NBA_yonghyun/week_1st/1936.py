f8aed438869ceb3353ec4da7794c7d7fc1782d22
yonghyunc <chyh9218@gmail.com>
2022-08-04 20:50:12 +0900

    Feat: 1936번 문제 해결

--- NBA_yonghyun/week_1st/1936.py
+++ NBA_yonghyun/week_1st/1936.py
@@ -1,1 +1,15 @@
 # 1대1 가위바위보
+
+a, b = map(int, input().split())
+
+
+if a > 1:
+    if a == b + 1:
+        print("A")
+    else:
+        print("B")
+else:
+    if b == 3:
+        print("A")
+    else:
+        print("B")
