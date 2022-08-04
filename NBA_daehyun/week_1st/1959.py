20d5d440c8fd9b7aafa45e6774c88b239d250b0b
songdaehyun <goflwla92@naver.com>
2022-08-04 17:07:40 +0900

    1959번 문제 해결

--- NBA_daehyun/week_1st/1959.py
+++ NBA_daehyun/week_1st/1959.py
@@ -1,1 +1,22 @@
 # 두 개의 숫자열
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = []
+    repeat_num = abs(N - M) + 1
+    if N >= M:
+        for i in range(repeat_num):
+            inner_product = 0
+            for j in range(M):
+                inner_product += B[j] * A[j + i]
+            C.append(inner_product)
+    else:
+        for i in range(repeat_num):
+            inner_product = 0
+            for j in range(N):
+                inner_product += A[j] * B[j + i]
+            C.append(inner_product)
+    print(f'#{test_case} {max(C)}')
