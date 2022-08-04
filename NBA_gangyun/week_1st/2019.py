f30c89e921d0dbbe11d4e09dc1cb43e892cd66f4
이강윤 <lky61010@naver.com>
2022-08-04 23:49:35 +0900

    Feat: 2019번 문제 해결

--- NBA_gangyun/week_1st/2019.py
+++ NBA_gangyun/week_1st/2019.py
@@ -1,1 +1,19 @@
 # 더블더블
+
+# ---첫번째 제출---
+# num = int(input())
+# num_double = 1
+
+# for i in range(num + 1):
+#     if i == 0:
+#         num_double = 1
+#     else :
+#         num_double *= 2
+#     print(num_double, end=' ')
+
+a = int(input())
+b = 0.5
+
+for i in range(a + 1):
+    b *= 2
+    print(int(b), end=' ')
