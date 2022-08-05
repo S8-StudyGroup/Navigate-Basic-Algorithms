48b93a145c4458349bca23a5366740bc8a0c6e81
이강윤 <lky61010@naver.com>
2022-08-05 16:55:39 +0900

    Feat: 2019번 문제 주석 추가

--- NBA_gangyun/week_1st/2019.py
+++ NBA_gangyun/week_1st/2019.py
@@ -6,14 +6,14 @@
 
 # for i in range(num + 1):
 #     if i == 0:
-#         num_double = 1
+#         num_double = 1    # 2의 0제곱 == 1
 #     else :
-#         num_double *= 2
+#         num_double *= 2   # i가 반복한 횟수만큼 2가 곱해짐
 #     print(num_double, end=' ')
 
 a = int(input())
 b = 0.5
 
 for i in range(a + 1):
-    b *= 2
-    print(int(b), end=' ')
+    b *= 2  # if문을 없애기 위해 b를 '0.5*2 = 1'에서 시작
+    print(int(b), end=' ')  # 명시적 형변환 이용, float형 출력을 방지
