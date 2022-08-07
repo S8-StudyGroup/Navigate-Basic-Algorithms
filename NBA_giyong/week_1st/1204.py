65e356196b5be3a365f00154e282b93d70edbcb8
KI-DG <kanfs2022@gmail.com>
2022-08-07 12:09:00 +0900

    feat: 1204번 문제 해결

--- NBA_giyong/week_1st/1204.py
+++ NBA_giyong/week_1st/1204.py
@@ -1,1 +1,20 @@
 # 최빈수 구하기
+T = int(input()) 
+
+for tc in range(T):
+    tc = int(input())
+    score = list(map(int,input().split()))
+    data = [0]*1001
+
+    for i in score:
+        data[i] += 1
+
+    max_value = max(data)
+
+    result =[]
+
+    for i in range(len(data)):
+        if data[i]== max_value:
+            result.append(i)
+
+    print('#d% %d' % (tc,max(result)))
