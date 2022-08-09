8e3e3327cec2926324e3c4066149f1a1044db8c2
yonghyunc <chyh9218@gmail.com>
2022-08-09 20:34:50 +0900

    Feat: 1933번 문제 해결

--- NBA_yonghyun/week_2nd/1933.py
+++ NBA_yonghyun/week_2nd/1933.py
@@ -1,1 +1,22 @@
 # 간단한 N의 약수
+
+
+## 첫번째 코드
+
+N = int(input())
+
+index = 1
+
+while index <= N:
+    if N % index == 0:
+        print(index, end=' ')
+    index += 1
+
+
+## 두번째 코드
+
+N = int(input())
+
+for i in range(1, N + 1):
+    if N % i == 0:
+        print(i, end=' ')
