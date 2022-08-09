f8f3f7b9992e38d52218816261f467787355f79a
dokkyunYU <ydgar001@naver.com>
2022-08-09 20:43:48 +0900

    Feat: 1284번 문제 해결

--- NBA_deokgyun/week_1st/2019.py
+++ NBA_deokgyun/week_1st/2019.py
@@ -1,1 +1,5 @@
 # 더블더블
+
+N = int(input())
+
+print(*[2**i for i in range(N + 1)])
--- NBA_deokgyun/week_2nd/1284.py
+++ NBA_deokgyun/week_2nd/1284.py
@@ -1,1 +1,7 @@
 # 수도 요금 경쟁
+Test_count = int(input())
+for i in range(Test_count):
+    P, Q, R, S, W = map(int, input().split(" "))
+    A_water_fee = P * W
+    B_water_fee = Q if R > W else Q + S * (W - R)
+    print(f"#{i + 1}", A_water_fee if A_water_fee < B_water_fee else B_water_fee)
