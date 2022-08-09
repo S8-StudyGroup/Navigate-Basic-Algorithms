c6c909eccbf2cca59398990a9fef75365e405a84
yonghyunc <chyh9218@gmail.com>
2022-08-09 20:33:58 +0900

    Feat: 1284번 문제 해결

--- NBA_yonghyun/week_2nd/1284.py
+++ NBA_yonghyun/week_2nd/1284.py
@@ -1,1 +1,20 @@
 # 수도 요금 경쟁
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    p, q, r, s, w = map(int, input().split())
+
+    # 수도요금이 B사 기준 기본요금만 청구될 때
+    if w <= r:
+        if p * w > q:
+            print(f'#{test_case} {q}')
+        else:
+            print(f'#{test_case} {p * w}')
+
+    else:
+        if p * w > q + (w - r) * s:
+            print(f'#{test_case} {q + (w - r) * s}')
+        else:
+            print(f'#{test_case} {p * w}')
