81c7b48c904227da613cf438338bf7504b122b4a
KI-DG <kanfs2022@gmail.com>
2022-08-12 01:53:23 +0900

    Faet: 1938번 문제해결

--- NBA_giyong/week_1st/2019.py
+++ NBA_giyong/week_1st/2019.py
@@ -1,9 +1,10 @@
 # 더블더블
 
 n = int(input())
-1< n <=30
 
-
-for i in range(n+1):
-    print(2**i,end=' ')
+if 1<= n <=30:
+    for i in range(n+1):
+       print(2**i,end=' ')
+else:
+    print(-1)
     
