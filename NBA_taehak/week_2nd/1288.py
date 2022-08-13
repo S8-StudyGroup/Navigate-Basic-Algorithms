97e0d3b1a3318c73538645d80e20998bb111eca7
0happydeath0 <0happydeath0@naver.com>
2022-08-14 03:14:44 +0900

    Feat: 1288번 문제해결

--- NBA_taehak/week_2nd/1288.py
+++ NBA_taehak/week_2nd/1288.py
@@ -1,1 +1,19 @@
 # 새로운 불면증 치료법
+
+import sys
+
+sys.stdin = open('input_1288.txt')
+
+for case in range(1, int(input()) + 1):
+    n = int(input())
+
+    sheep = 0
+    zero_ten = '1234567890'
+
+    while zero_ten != '':
+        sheep += 1
+        n_sheep = n * sheep
+        for i in str(n_sheep):
+            zero_ten = zero_ten.replace(i, '')
+
+    print(f'#{case} {n_sheep}')
