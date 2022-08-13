6ef77dd1913a6088a5f0a3b81d88fff276847753
0happydeath0 <0happydeath0@naver.com>
2022-08-14 04:20:22 +0900

    Feat: 1946번 문제해결

--- NBA_taehak/week_3rd/1946.py
+++ NBA_taehak/week_3rd/1946.py
@@ -1,1 +1,17 @@
 # 간단한 압축풀기
+
+import sys
+
+sys.stdin = open('input_1946.txt')
+
+for case in range(1, int(input()) + 1):
+    temp = ''
+    for j in range(int(input())):
+        abc, num = input().split()
+        temp += abc * int(num)
+
+    print(f'#{case}')
+
+    while len(temp) > 0:
+        print(temp[:10])
+        temp = temp[10:]
