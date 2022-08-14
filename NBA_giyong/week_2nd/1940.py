3ce45579e514aa59514ea515a6723eaf00896950
KI-DG <kanfs2022@gmail.com>
2022-08-14 20:13:33 +0900

    Feat: 1940번 문제해결

--- NBA_giyong/week_2nd/1940.py
+++ NBA_giyong/week_2nd/1940.py
@@ -1,1 +1,20 @@
 # 가랏! RC카!
+t = int(input())
+
+for tc in range(1, t + 1):
+    n = int(input())
+    m = 0  # 이동한 거리
+    m_s = 0
+    for i in range(n):
+        s = list(map(int, input().split()))
+        if s[0] == 1:
+            m_s += s[1]
+            m += m_s
+        elif s[0] == 2:
+            m_s -= s[1]
+            if m_s < 0:
+                m_s = 0
+            m += m_s
+        else:
+            m += m_s
+    print(f"#{tc} {m}")
