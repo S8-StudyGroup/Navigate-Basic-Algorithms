ef79164ac110a67ac7dbcfae03388eb4f31bc291
ssg-js <likemath1997@gmail.com>
2022-08-14 19:32:27 +0900

    Feat: 1940 문제 해결

--- NBA_jinseong/week_1st/2019.py
+++ NBA_jinseong/week_1st/2019.py
@@ -2,7 +2,6 @@
 num = int(input())
 
 result = 1
-for n in range(num):  # num 수 만큼 * 2
+for n in range(num+1):  # num 수 만큼 * 2
     print(result, end=' ')
     result *= 2
-print(result)  # 마지막 * 2 후 for문을 나오므로 마지막숫자 출력
--- NBA_jinseong/week_2nd/1940.py
+++ NBA_jinseong/week_2nd/1940.py
@@ -1,1 +1,29 @@
 # 가랏! RC카!
+
+for t in range(1, int(input()) + 1):
+    rc_state = 0
+    distance = 0
+    for n in range(1, int(input()) + 1):
+        li = list(map(int, input().split()))
+        if 0 < li[0] <= 2:
+            rc_change, acc = li[0:2]
+        elif li[0] == 0:
+            rc_change = li[0]
+        else:
+            continue
+
+        if rc_change == 0:
+            distance += rc_state
+        elif rc_change == 1:
+            rc_state += acc
+            distance += rc_state
+        elif rc_change == 2:
+            if rc_state > acc:
+                rc_state -= acc
+                distance += rc_state
+            else:  # 감속값이 현 속도값보다 작으면
+                rc_state = 0
+        else:
+            pass
+
+    print(f'#{t} {distance}')
