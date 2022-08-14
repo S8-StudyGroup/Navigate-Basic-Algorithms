27afc10b034669507bfad7190766593e364e577c
KI-DG <kanfs2022@gmail.com>
2022-08-14 20:07:17 +0900

    Feat: 1284번 문제해결

--- NBA_giyong/week_2nd/1284.py
+++ NBA_giyong/week_2nd/1284.py
@@ -1,1 +1,23 @@
 # 수도 요금 경쟁
+import sys
+
+sys.stdin = open('input.txt')
+
+t = int(input())
+
+for tc in range(1, t + 1):
+    p, q, r, s, w = map(int, input().split())
+
+    a = 0
+    b = 0
+    if w > r:
+        a += p * w
+        b += q + (w - r)*s
+    else:
+        a += p * w
+        b += q
+
+    if a < b:
+        print(f"#{tc} {a}")
+    else:
+        print(f"#{tc} {b}")
--- NBA_giyong/week_2nd/1288.py
+++ NBA_giyong/week_2nd/1288.py
@@ -1,1 +1,9 @@
 # 새로운 불면증 치료법
+import sys
+
+sys.stdin = open("input")
+
+t = int(input())
+
+for tc in range(1, t + 1):
+    pass
