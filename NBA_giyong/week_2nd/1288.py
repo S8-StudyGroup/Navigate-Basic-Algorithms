49933e7aada8ba836104e2141c54f0cf076e491f
KI-DG <kanfs2022@gmail.com>
2022-08-14 20:16:43 +0900

    Faet: 1288번 문제해결

--- NBA_giyong/week_2nd/1288.py
+++ NBA_giyong/week_2nd/1288.py
@@ -1,9 +1,18 @@
 # 새로운 불면증 치료법
 import sys
 
-sys.stdin = open("input")
+sys.stdin = open("input.txt")
 
 t = int(input())
 
 for tc in range(1, t + 1):
-    pass
+    n = input()
+    value = int(n)
+    data = [0]*10 
+    while True:
+        for i in range(len(n)):
+            data[int(n[i])] += 1
+        if not data.count(0):  
+            print(f"#{tc} {int(n)}")
+            break   
+        n = str(value + int(n))
