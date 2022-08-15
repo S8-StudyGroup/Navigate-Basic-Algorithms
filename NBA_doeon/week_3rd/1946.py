558b676f7357cb3b68a12964f2abe67fff73c54a
김도언 <vov06144@naver.com>
2022-08-15 22:03:24 +0900

    Feat: 1946번 문제 해결

--- NBA_doeon/week_3rd/1946.py
+++ NBA_doeon/week_3rd/1946.py
@@ -1,1 +1,20 @@
 # 간단한 압축풀기
+
+t = int(input())
+
+for tc in range(1, t + 1):
+    n = int(input())
+    words = ''
+
+    print()
+    print(f'#{tc}')
+    
+    for i in range(n):
+        letter, number = input().split()
+        words += letter * int(number)
+
+    for j in range(1, len(words) + 1):
+        if j % 10 == 0:
+            print(words[j - 1])
+        else:
+            print(words[j - 1], end='')
