2920342c3fc8fb84f6a19dc2aa9959f1af5d8538
HeeJeYang <ghjadmb21@gmail.com>
2022-08-16 20:49:58 +0900

    Feat: 1946번 문제 해결

--- NBA_heeje/week_3rd/1946.py
+++ NBA_heeje/week_3rd/1946.py
@@ -1,1 +1,13 @@
 # 간단한 압축풀기
+T = int(input())
+for test_case in range(1, T + 1):
+    A = int(input())
+    ans = ""
+    for i in range(A):
+        alp, count = map(str, input().split())
+        for j in range(int(count)):
+            ans += alp
+    print(f"#{test_case}")
+    while len(ans) > 0:
+        print(ans[0:10])
+        ans = ans[10:]
