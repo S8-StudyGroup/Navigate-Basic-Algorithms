d584f80122fdecc87a3114427dfe3752a7882f33
yonghyunc <chyh9218@gmail.com>
2022-08-16 20:36:09 +0900

    Feat: 2027번 문제 해결

--- NBA_yonghyun/week_3rd/2027.py
+++ NBA_yonghyun/week_3rd/2027.py
@@ -1,1 +1,22 @@
 # 대각선 출력하기
+
+'''
+[ 아이디어 ]
+
+
+insert + for문으로 위치 지정
+
+언패킹으로 출력
+
+다시 삭제
+
+부호 사이에 띄어쓰기 없어야 함 =>  sep=''
+
+'''
+
+plus = ['+'] * 4
+
+for i in range(5):
+    plus.insert(i, '#')
+    print(*plus, sep='')
+    plus.pop(i)
