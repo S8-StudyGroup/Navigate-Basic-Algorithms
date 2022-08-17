f051d3db0d16ad4499a02c455d8eae7532da3fd1
KI-DG <kanfs2022@gmail.com>
2022-08-17 19:55:36 +0900

    Feat: 1946번 문제해결

--- NBA_giyong/week_3rd/1946.py
+++ NBA_giyong/week_3rd/1946.py
@@ -1,1 +1,20 @@
 # 간단한 압축풀기
+import sys
+
+sys.stdin = open("input.txt")
+
+t = int(input())
+
+for tc in range(1, t + 1):
+    n = int(input())
+    answer = ''
+    for i in range(n):
+        data = input().split()
+        answer += data[0] * int(data[1])
+
+    print(f"#{tc}")
+    for i in range(0, len(answer), 10):
+        if len(answer) > 10:
+            print(answer[i:i+10], end='\n')
+        else:
+            print(answer[i:i+10])
