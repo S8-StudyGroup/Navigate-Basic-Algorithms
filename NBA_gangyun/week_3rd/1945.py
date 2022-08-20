56e0628c820a6bcd80bbc2e99e6760603c48fd3c
이강윤 <lky61010@naver.com>
2022-08-20 21:18:47 +0900

    Feat: 1945번 문제 해결

--- NBA_gangyun/week_3rd/1945.py
+++ NBA_gangyun/week_3rd/1945.py
@@ -1,1 +1,18 @@
 # 간단한 소인수분해
+
+import sys
+sys.stdin = open('input.txt')
+
+
+def func(divider, N):
+    count = 0
+    while N % divider == 0:
+        N /= divider
+        count += 1
+    return count
+
+
+for test_case in range(1, int(input()) + 1):
+    N = int(input())
+    print(f'#{test_case} {func(2, N)} {func(3, N)} {func(5, N)} {func(7, N)} {func(11, N)}')
+
