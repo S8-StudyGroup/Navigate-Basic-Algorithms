6efc3d605c9041eed9652ff8e3494bc49b1160a3
이강윤 <lky61010@naver.com>
2022-08-20 22:23:53 +0900

    Feat: 1946번 문제 해결

--- NBA_gangyun/week_3rd/1946.py
+++ NBA_gangyun/week_3rd/1946.py
@@ -1,1 +1,14 @@
 # 간단한 압축풀기
+
+for test_case in range(1, int(input()) + 1):
+    N = int(input())
+    word_zip = ''
+    print_word = ''
+    for _ in range(N):
+        word_info = list(input().split())
+        word_zip += word_info[0] * int(word_info[1])
+
+    print(f'#{test_case}')
+    for i in range(0, len(word_zip)):
+        if i % 10 == 0:
+            print(word_zip[i : i + 10 :])
