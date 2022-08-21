7b87b0a14d651e99b3e197f1744f83495cbf30b3
ssg-js <likemath1997@gmail.com>
2022-08-21 18:57:49 +0900

    Feat: 1945번 문제 해결

--- NBA_jinseong/week_3rd/1945.py
+++ NBA_jinseong/week_3rd/1945.py
@@ -1,1 +1,19 @@
+
 # 간단한 소인수분해
+
+for t in range(1, int(input()) + 1):
+    n = int(input())
+    small_num = {2: 0, 3: 0, 5: 0, 7: 0, 11: 0}
+
+    for num in small_num.keys():
+        cnt = 0
+        while True:
+            # 소인수로 나눠지면 나눈 몫을 저장하고 카운트
+            if n % num == 0:
+                n /= num
+                cnt += 1
+            else:
+                break
+        small_num[num] = cnt
+
+    print(f'#{t}', *small_num.values())
