f6a3a55a554a5e55e9f991fe673c94076eee2c21
HeeJeYang <ghjadmb21@gmail.com>
2022-08-21 23:23:14 +0900

    Docs: 3주차 1팀 회의 관련 코드 개선 및 주석 추가

--- NBA_heeje/week_3rd/1946.py
+++ NBA_heeje/week_3rd/1946.py
@@ -1,12 +1,20 @@
 # 간단한 압축풀기
+
 T = int(input())
+
 for test_case in range(1, T + 1):
+
     A = int(input())
     ans = ""
+
     for i in range(A):
-        alp, count = map(str, input().split())
-        for j in range(int(count)):
-            ans += alp
+        # 알파벳과 개수는 자료형이 다르므로 주의!
+        alp, count = input().split()
+
+        # count가 현재 문자열이므로 int로 변환
+        ans += alp * int(count)
+
+    # 10개씩 끊어서 출력
     print(f"#{test_case}")
     while len(ans) > 0:
         print(ans[0:10])
