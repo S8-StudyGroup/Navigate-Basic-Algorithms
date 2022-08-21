839108442769a9ec4950db6338a07ac016a51715
ssg-js <likemath1997@gmail.com>
2022-08-21 19:02:31 +0900

    Feat: 1946번 문제 해결

--- NBA_jinseong/week_3rd/1946.py
+++ NBA_jinseong/week_3rd/1946.py
@@ -1,1 +1,23 @@
 # 간단한 압축풀기
+
+for t in range(1, int(input()) + 1):
+    n = int(input())
+    contents = []
+    c = []
+    k = []
+    for i in range(n):
+        f = input()
+        contents = f.split()
+        c.append(contents[0])
+        k.append(int(contents[1]))
+
+    count = 0  # 10개 출력시 다음 줄로 변경
+    print(f'#{t}')
+    for i, v in enumerate(k):
+        for j in range(v):
+            if count == 10:
+                print()
+                count = 0
+            print(c[i], end='')
+            count += 1
+    print()
