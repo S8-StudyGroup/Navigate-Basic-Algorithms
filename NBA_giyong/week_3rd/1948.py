00ed13249748430d40ce2a8bb18ec62a673a78b1
KI-DG <kanfs2022@gmail.com>
2022-08-21 20:44:13 +0900

    Feat: 1948번 문제 해결

--- NBA_giyong/week_3rd/1948.py
+++ NBA_giyong/week_3rd/1948.py
@@ -1,1 +1,20 @@
 # 날짜 계산기
+# 날짜 계산기
+import sys
+
+sys.stdin = open("input.txt")
+
+data = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t = int(input())
+for tc in range(1, t + 1):
+    month, day, next_month, next_day = map(int, input().split())
+    result = 0
+    if month == next_month:
+        result += next_day - day + 1
+    else:
+        result += next_day
+        for j in range(next_month-1, month, -1):
+            result += data[j]
+        result += data[month] - day + 1
+
+    print(f"#{tc} {result}")
