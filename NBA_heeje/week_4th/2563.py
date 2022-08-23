3040c72fa60445e916dae53a3a411547d926a2c8
HeeJeYang <ghjadmb21@gmail.com>
2022-08-23 20:15:13 +0900

    Feat: 2563번 문제 해결

--- NBA_heeje/week_4th/2563.py
+++ NBA_heeje/week_4th/2563.py
@@ -1,1 +1,15 @@
 # 백준 - 색종이
+
+board = [[0 for _ in range(100)] for _ in range(100)]
+cnt = 0
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+
+    for j in range(y, y + 10):
+        for i in range(x, x + 10):
+            if board[j][i] == 0:
+                board[j][i] = 1
+                cnt += 1
+
+print(cnt)
