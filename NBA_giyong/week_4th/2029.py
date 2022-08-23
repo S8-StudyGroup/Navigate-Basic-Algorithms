346d009536b950151c49c5e6d647b0ce185b4e00
KI-DG <kanfs2022@gmail.com>
2022-08-24 00:05:43 +0900

    Feat: 2029번 문제 해결

--- NBA_giyong/week_4th/2029.py
+++ NBA_giyong/week_4th/2029.py
@@ -1,1 +1,15 @@
 # 몫과 나머지 출력하기
+
+import sys
+
+sys.stdin = open("input")
+
+t = int(input())
+
+for tc in range(1, t + 1):
+    a, b = map(int, input().split())
+    share = a // b
+    remainder = a % b
+
+    print(f"#{tc}", end=" ")
+    print(share, remainder)
