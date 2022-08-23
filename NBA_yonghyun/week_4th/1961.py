4e4aa814ccdc624ae848bd0fd7dff9b34e5beb5f
yonghyunc <chyh9218@gmail.com>
2022-08-23 19:58:29 +0900

    Feat: 1961번 문제 해결

--- NBA_yonghyun/week_4th/1961.py
+++ NBA_yonghyun/week_4th/1961.py
@@ -1,1 +1,25 @@
 # 숫자 배열 회전
+
+'''
+저번 수업 때 다뤘던 문제
+zip 함수 사용하면 쉽게 풀 수 있음
+'''
+
+t = int(input())
+
+for test_case in range(1, t + 1):
+    n = int(input())
+    arr = [input().replace(' ', '') for _ in range(n)]
+
+    # 90도
+    arr2 = list(map(''.join, zip(*arr)))
+
+    # 180도
+    arr3 = [arr[i][-1::-1] for i in range(n - 1, -1, -1)]
+
+    # 270도
+    arr4 = list(map(''.join, zip(*arr3)))
+
+    print(f'#{test_case}')
+    for i in range(n):
+        print(arr2[i][::-1], arr3[i], arr4[i][::-1])
