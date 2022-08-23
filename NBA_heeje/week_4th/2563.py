df88fdc09cbc2b8e98a08089c722be92c840841d
HeeJeYang <ghjadmb21@gmail.com>
2022-08-23 20:53:49 +0900

    Docs: 2563번 문제 주석 추가

--- NBA_heeje/week_4th/2563.py
+++ NBA_heeje/week_4th/2563.py
@@ -6,8 +6,10 @@ cnt = 0
 for _ in range(int(input())):
     x, y = map(int, input().split())
 
+    # 2차원 좌표를 푸는 관점으로 x, y를 생각!
     for j in range(y, y + 10):
         for i in range(x, x + 10):
+            # 이미 검은 영역인 곳은 제외하고 흰 영역만 검게 칠해주며 카운트한다.
             if board[j][i] == 0:
                 board[j][i] = 1
                 cnt += 1
