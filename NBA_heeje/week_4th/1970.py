b4f846596ce5ac1f4f52b3decab43e732e38df53
HeeJeYang <ghjadmb21@gmail.com>
2022-08-23 20:52:55 +0900

    Feat: 1970번 문제 해결

--- NBA_heeje/week_4th/1970.py
+++ NBA_heeje/week_4th/1970.py
@@ -1,1 +1,17 @@
 # 쉬운 거스름돈
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+
+    N = int(input())
+
+    total = []
+    money_list = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+
+    for money in money_list:
+        total.append(N // money)
+        N = N % money
+
+    print(f"#{test_case}")
+    print(*total)
