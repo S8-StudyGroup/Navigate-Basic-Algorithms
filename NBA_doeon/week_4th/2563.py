6de57325101a6dfda166eb3b5b26b4a8fa9d6244
DOEON KIM <vov06144@naver.com>
2022-08-23 17:41:00 +0900

    Feat: 2563번 문제 해결

--- NBA_doeon/week_4th/2563.py
+++ NBA_doeon/week_4th/2563.py
@@ -1,1 +1,18 @@
 # 백준 - 색종이
+
+board = [[0] * 100 for _ in range(100)]  # 흰 도화지 board
+
+num = int(input())
+
+cnt = 0  # 검은색 칸 수
+
+for paper in range(num):  # 색종이 개수마다
+    a, b = map(int, input().split())  # 색종이의 왼쪽 상단, 오른쪽 하단 위치를 받음
+
+    for i in range(100 - b - 10, 100 - b):
+        for j in range(a, a + 10):
+            if board[i][j] == 0:  # 이미 검은색으로 겹쳐진 부분은 무시하고 겹쳐지지 않은 범위를
+                board[i][j] = 1  # 1로 표시해준 후
+                cnt += 1  # 한 칸을 + 1
+
+print(cnt)
