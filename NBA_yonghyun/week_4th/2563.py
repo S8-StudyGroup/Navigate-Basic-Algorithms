9acd79d9df82e01d62b1a79c7544e2430d17d164
yonghyunc <chyh9218@gmail.com>
2022-08-23 20:01:31 +0900

    Feat: 2563번 문제 해결

--- NBA_yonghyun/week_4th/2563.py
+++ NBA_yonghyun/week_4th/2563.py
@@ -1,1 +1,26 @@
 # 백준 - 색종이
+
+'''
+도화지를 위아래로 뒤집은 상태라고 생각하고 풀면 인덱스를 굳이 안 바꿔도 됨
+만약 입력이 3 7 이면
+[6][2]이 왼쪽 위 꼭짓점인 10x10 정사각형이라고 생각하고 풀면 됨
+'''
+
+white = [[0] * 100 for _ in range(100)]  # 흰색 도화지 - 칠하지 않은 부분은 0
+t = int(input())  # 색종이의 수
+
+for t in range(t):
+    y, x = map(int, input().split())
+
+    for i in range(x - 1, x + 9):  # 행 10번 반복
+        for j in range(y - 1, y + 9):  # 열 10번 반복
+            white[i][j] = 1  # 색종이의 영역은 1로 바꿈
+
+# 색종이의 영역 계산
+cnt = 0
+for i in range(100):
+    for j in range(100):
+        if white[i][j] == 1:
+            cnt += 1
+
+print(cnt)
