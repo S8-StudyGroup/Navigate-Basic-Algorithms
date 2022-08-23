efa22a7b2ff33361fcacd5f3aa745dc6b363271c
yonghyunc <chyh9218@gmail.com>
2022-08-23 20:00:51 +0900

    Feat: 2043번 문제 해결

--- NBA_yonghyun/week_4th/2043.py
+++ NBA_yonghyun/week_4th/2043.py
@@ -1,1 +1,15 @@
 # 서랍의 비밀번호
+
+p, k = map(int, input().split())
+
+print(p - k + 1)
+
+
+# -------------------
+
+# p 가 k 보다 무조건 크다는 조건이 문제에 명시되어 있지 않음
+
+if p >= k:
+    print(p - k + 1)
+else:
+    print(999 - k + p + 2)
