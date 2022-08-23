39cd37d0f69e1c46c95d5c42fceb656a61a7d478
yonghyunc <chyh9218@gmail.com>
2022-08-23 19:59:09 +0900

    Feat: 1966번 문제 해결

--- NBA_yonghyun/week_4th/1966.py
+++ NBA_yonghyun/week_4th/1966.py
@@ -1,1 +1,15 @@
 # 숫자를 정렬하자
+
+t = int(input())
+
+for test_case in range(1, t + 1):
+    n = int(input())
+
+    nums = list(map(int, input().split()))
+
+    for _ in range(n):
+        for i in range(n - 1, 0, -1):
+            if nums[i] < nums[i - 1]:
+                nums[i], nums[i - 1] = nums[i - 1], nums[i]
+
+    print(f'#{test_case}', *nums)
