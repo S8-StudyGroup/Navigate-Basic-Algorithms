caa0a37fe1761e5296e393df1da830654f4642af
ssg-js <likemath1997@gmail.com>
2022-08-26 10:18:42 +0900

    Feat: 1961번 문제 해결

--- NBA_jinseong/week_4th/1961.py
+++ NBA_jinseong/week_4th/1961.py
@@ -1,1 +1,19 @@
 # 숫자 배열 회전
+
+for t in range(1, int(input()) + 1):
+    n = int(input())
+
+    li = [list(map(int, input().split())) for _ in range(n)]
+
+    print(f'#{t}')
+    for i in range(n):
+        for j in range(n):
+            print(li[n-1-j][i], end='')
+        print(end=' ')
+        for j in range(n):
+            print(li[n-1-i][n-1-j], end='')
+        print(end=' ')
+        for j in range(n):
+            print(li[j][n-1-i], end='')
+        print()
+
