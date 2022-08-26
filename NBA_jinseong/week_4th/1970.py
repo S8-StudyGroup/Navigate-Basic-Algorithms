7584c994685b4bf398c2c50873576682ad4f4407
ssg-js <likemath1997@gmail.com>
2022-08-26 10:36:26 +0900

    Feat: 1970번 문제 해결

--- NBA_jinseong/week_4th/1970.py
+++ NBA_jinseong/week_4th/1970.py
@@ -1,1 +1,15 @@
 # 쉬운 거스름돈
+
+money = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+
+for t in range(1, int(input()) + 1):
+    price = int(input())
+
+    change = [0] * 8
+
+    for i, v in enumerate(money):
+        change[i] = price // v
+        price = price % v
+
+    print(f'#{t}')
+    print(*change)
