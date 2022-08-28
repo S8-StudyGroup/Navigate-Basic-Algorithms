6e2fc19f65d85a1d159b4ceb20f338bcb7a80ba6
KI-DG <kanfs2022@gmail.com>
2022-08-28 20:50:10 +0900

    Feat: 2563번 문제 해결

--- NBA_giyong/week_4th/2563.py
+++ NBA_giyong/week_4th/2563.py
@@ -1,1 +1,21 @@
 # 백준 - 색종이
+import sys
+
+sys.stdin = open('input')
+
+
+paper = int(input())  # 색종이의 수
+n = 100
+
+arr = [[0] * n for _ in range(n)]
+count = 0
+for _ in range(paper):
+    x, y = map(int, input().split())
+
+    for i in range(y, y + 10):
+        for j in range(x, x + 10):
+            if arr[i][j] != 1:
+                arr[i][j] = 1
+                count += 1
+
+print(count)
