c898d35bfef9d0237f4f429ba81267cb9bb26bdb
dokkyunYU <ydgar001@naver.com>
2022-08-30 17:08:40 +0900

    Feat: 1983번 문제 해결

--- NBA_deokgyun/week_5th/1976.py
+++ NBA_deokgyun/week_5th/1976.py
@@ -1,1 +1,9 @@
 # 시각 덧셈
+
+for test_case in range(1, int(input()) + 1):
+    clock_list = list(map(int, input().split()))
+    print(
+        f"#{test_case}",
+        sum(clock_list[::2]) % 12 + sum(clock_list[1::2]) // 60,
+        sum(clock_list[1::2]) % 60,
+    )
--- NBA_deokgyun/week_5th/1983.py
+++ NBA_deokgyun/week_5th/1983.py
@@ -1,1 +1,17 @@
 # 조교의 성적 매기기
+
+import sys
+
+sys.stdin = open("input.txt")
+
+for test_case in range(1, int(input()) + 1):
+    student_amount, what_grade = map(int, input().split())
+    all_scores = []
+    for _ in range(student_amount):
+        i, j, k = map(int, input().split())
+        all_scores.append(i * (7 / 20) + j * (9 / 20) + k * (4 / 20))
+    want_to_know = all_scores[what_grade - 1]
+    all_scores.sort()
+    the_grade = all_scores.index(want_to_know) // (student_amount // 10)
+    grade_list = ['D0', 'C-', 'C0', 'C+', 'B-', 'B0', 'B+', 'A-', 'A0', 'A+']
+    print(f"#{test_case}", grade_list[the_grade])
