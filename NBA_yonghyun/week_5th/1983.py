d0a3f3c39cf0936e6e082a4136266c6118402500
yonghyunc <chyh9218@gmail.com>
2022-08-30 22:00:38 +0900

    Feat: 1983번 문제 해결

--- NBA_yonghyun/week_5th/1983.py
+++ NBA_yonghyun/week_5th/1983.py
@@ -1,1 +1,22 @@
 # 조교의 성적 매기기
+
+grade = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
+
+for test_case in range(1, int(input()) + 1):
+    n, k = map(int, input().split())
+    students = [list(map(int, input().split())) for _ in range(n)]
+
+    scores = [0] * n
+    for i in range(n):
+        scores[i] = 0.35 * students[i][0] + 0.45 * students[i][1] + 0.2 * students[i][2]
+        if i == k - 1:
+            k_score = scores[i]
+    cut = n // 10
+    scores = sorted(scores, reverse=True)
+
+    idx = 0
+    for i in range(0, n, cut):
+        if k_score in scores[i : i + cut]:
+            print(f'#{test_case} {grade[idx]}')
+            break
+        idx += 1
