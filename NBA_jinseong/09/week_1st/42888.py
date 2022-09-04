7a04c1b556eb6b65cffe57b912525df67b97c704
ssg-js <likemath1997@gmail.com>
2022-09-04 20:37:23 +0900

    Feat: 42888번 문제 해결

--- NBA_jinseong/09/week_1st/42888.py
+++ NBA_jinseong/09/week_1st/42888.py
@@ -1,1 +1,31 @@
 # 42888. 오픈채팅방
+
+
+def solution(record):
+    answer = []  # 최종출력
+    codes = []  # 유저 id 값
+    names = dict()  # id 당 유저 이름
+    status = []  # 나갔는지 들어왔는지
+    for index, text in enumerate(record):
+        text_ls = text.split()
+        word = text_ls[0]
+        code = text_ls[1]
+        if len(text_ls) == 3:
+            user = text_ls[2]
+
+        if word == 'Enter':
+            codes.append(code)
+            status.append('님이 들어왔습니다.')
+            names[code] = user
+        elif word == 'Leave':
+            codes.append(code)
+            status.append('님이 나갔습니다.')
+        elif word == 'Change':
+            names[code] = user
+
+    for i, code in enumerate(codes):
+        answer.append(names[code]+status[i])
+
+    return answer
+
+
