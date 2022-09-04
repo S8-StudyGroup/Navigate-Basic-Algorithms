225612880a8138f4eb9674c979e9a7ca26bfca1e
이강윤 <lky61010@naver.com>
2022-09-04 15:51:49 +0900

    Feat: 2635번 문제 해결

--- NBA_gangyun/09/week_1st/2635.py
+++ NBA_gangyun/09/week_1st/2635.py
@@ -1,1 +1,19 @@
 # 2635. 수 이어가기
+
+num = int(input())
+max_length = []
+
+# range(1, num)에서 반복을 돌릴경우 num = 1일 때 테스트 케이스가 틀림
+for a_2 in range(1, num + 1):
+    a_1 = num
+    nums = [a_1, a_2]
+    while a_1 - a_2 >= 0:
+        a_3 = a_1 - a_2
+        a_1, a_2 = a_2, a_3
+        nums.append(a_3)
+    if len(nums) > len(max_length):
+        max_length = nums
+
+print(len(max_length))
+print(*max_length)
+
