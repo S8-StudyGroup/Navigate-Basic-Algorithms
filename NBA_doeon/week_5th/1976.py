fc73c8fdcc735db3fdfa56c372ac6e9c9bb81296
김도언 <vov06144@naver.com>
2022-09-05 20:21:51 +0900

    Docs: 1976번, 1979번 문제 주석 수정

--- NBA_doeon/week_5th/1976.py
+++ NBA_doeon/week_5th/1976.py
@@ -6,7 +6,9 @@ for tc in range(1, t + 1):
     a_h, a_m, b_h, b_m = map(int, input().split())
     
     hour = (a_h + b_h) % 12
-    min = (a_m + b_m) % 60
     hour += (a_m + b_m) // 60
+
+    minute = (a_m + b_m) % 60
     
-    print(f'#{tc} {hour} {min}')
+    
+    print(f'#{tc} {hour} {minute}')
--- NBA_doeon/week_5th/1979.py
+++ NBA_doeon/week_5th/1979.py
@@ -4,9 +4,7 @@ t = int(input())
 
 for tc in range(1, t + 1):
     n, k = map(int, input().split())  # 행렬의 크기 N, 글자수 K
-    arr = [
-        list(map(int, input().split())) for _ in range(n)
-    ]  # 0,1로 이뤄진 행렬 (0 : 벽, 1 : 단어가 들어갈 수 있는 공간)
+    arr = [list(map(int, input().split())) for _ in range(n)]  # 0,1로 이뤄진 행렬 (0 : 벽, 1 : 단어가 들어갈 수 있는 공간)
     k_cnt = 0  # 단어가 들어갈 수 있는 공간 개수 k_cnt
 
     # 1) 행 방향으로 연속되는 1 찾기
@@ -21,9 +19,11 @@ for tc in range(1, t + 1):
                     k_cnt += 1  # 만약 달성했다면 단어 개수 k_cnt + 1
                 cnt = 0  # k개를 달성하지 못했다면 cnt를 초기화
 
+        ## 오른쪽, 아래 칸을 0으로 둘러쌓이게 이차원 리스트를 조작하면 이 부분이 필요 없음! - 아래 코드 반복되는 게 복잡할수록 효율적일듯!
         if cnt == k:  # 하나의 행을 다 돌았는데 중간에 0이 끊기지 않은 채로 끝났다면
             k_cnt += 1  # k개가 올바르게 연속됐으므로 k_cnt + 1
 
+        
     # 2) 열 방향으로 연속되는 1 찾기
     for j in range(n):
         cnt = 0
