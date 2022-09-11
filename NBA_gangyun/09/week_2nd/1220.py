75ca9226ad5f313ee586c04bf67ddf4c9b38dfad
이강윤 <lky61010@naver.com>
2022-09-11 21:09:07 +0900

    Feat: 1220번 문제 해결

--- NBA_gangyun/09/week_2nd/1220.py
+++ NBA_gangyun/09/week_2nd/1220.py
@@ -1,1 +1,26 @@
 # 1220. Magnetic
+
+import sys
+
+sys.stdin = open('1220input.txt')
+
+for test_case in range(1, 11):
+    num = int(input())
+    table = [list(map(int, input().split())) for _ in range(num)]
+    count = 0
+
+    for i in range(num):
+        row_count = 0
+        crash_counter = []
+        for j in range(num):
+            if table[j][i] == 1:
+                crash_counter.append(1)
+            elif table[j][i] == 2:
+                crash_counter.append(2)
+
+        for i in range(len(crash_counter) - 1):
+            if crash_counter[i] == 1 and crash_counter[i + 1] == 2:
+                count += 1
+
+    print(f'#{test_case} {count}')
+    
