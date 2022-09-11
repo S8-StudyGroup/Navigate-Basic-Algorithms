5744f3bc4c58acfa95f590cada2085d0df21061f
0happydeath0 <0happydeath0@naver.com>
2022-09-12 01:51:02 +0900

    Feat: 60058번 문제 해결

--- NBA_taehak/09/week_2nd/60058.py
+++ NBA_taehak/09/week_2nd/60058.py
@@ -1,5 +1,6 @@
 # 60058. 괄호 변환
 
+
 def is_correct(p):
     stack = []
 
@@ -61,3 +62,25 @@ def solution(p):
         u = reverse(u[1:-1])
         answer += u
         return answer
+
+
+# # 다른사람풀이
+# def solution(p):
+#     if p=='':
+#         return p
+
+#     r=True
+#     c=0
+
+#     for i in range(len(p)):
+#         if p[i]=='(':
+#             c-=1
+#         else:
+#             c+=1
+#         if c>0:
+#             r=False
+#         if c==0:
+#             if r:
+#                 return p[:i+1]+solution(p[i+1:])
+#             else:
+#                 return '('+solution(p[i+1:])+')'+''.join(list(map(lambda x:'(' if x==')' else ')',p[1:i]) ))
