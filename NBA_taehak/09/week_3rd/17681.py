ba032513f51736f2342b93541cc70ac7adc99ab9
0happydeath0 <0happydeath0@naver.com>
2022-09-18 18:59:52 +0900

    Feat: 17681번 문제해결

--- NBA_taehak/09/week_3rd/17681.py
+++ NBA_taehak/09/week_3rd/17681.py
@@ -27,10 +27,7 @@ def solution2(n, arr1, arr2):
 
     for i in range(n):
         row = bin(arr1[i] | arr2[i])[2:]
-
-        while len(row) < 5:
-            row = ' ' + row
-
+        row = ' ' * (n - len(row)) + row
         row = row.replace('0', ' ')
         answer.append(row.replace('1', '#'))
 
@@ -39,8 +36,8 @@ def solution2(n, arr1, arr2):
 
 # test
 
-# n = 5
-# arr1 = [9, 20, 28, 18, 11]
-# arr2 = [30, 1, 21, 17, 28]
-# print(solution(n, arr1, arr2))
-# print(solution2(n, arr1, arr2))
+n = 6
+arr1 = [46, 33, 33, 22, 31, 50]
+arr2 = [27, 56, 19, 14, 14, 10]
+print(solution(n, arr1, arr2))
+print(solution2(n, arr1, arr2))
