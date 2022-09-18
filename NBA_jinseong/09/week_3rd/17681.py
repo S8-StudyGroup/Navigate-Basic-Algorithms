989ce0266a0af02025bf61adba29bc3297b51dab
ssg-js <likemath1997@gmail.com>
2022-09-18 19:44:42 +0900

    Feat: 17681번 문제 해결

--- NBA_jinseong/09/week_3rd/17681.py
+++ NBA_jinseong/09/week_3rd/17681.py
@@ -1,1 +1,19 @@
 # [Programmers] 17681. [1차] 비밀지도
+
+
+def solution(n, arr1, arr2):
+    whole_map = []
+    for i in range(n):
+        whole_map.append(int(arr1[i]) | int(arr2[i]))
+
+    print(whole_map)
+    answer = []
+    for i in range(n):
+        binary = bin(whole_map[i])[2:n+2]
+        if len(binary) != n:
+            binary = '0' * (n - len(binary)) + binary
+        answer.append(binary.replace('1', '#').replace('0', ' '))
+
+    return answer
+
+# print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
