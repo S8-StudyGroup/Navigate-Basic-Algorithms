98ffe319550143631371908b8fdcdf0b8268c34f
dokkyunYU <ydgar001@naver.com>
2022-09-20 21:08:15 +0900

    Feat: 17681번 문제해결

--- NBA_deokgyun/09/week_3rd/17681.py
+++ NBA_deokgyun/09/week_3rd/17681.py
@@ -6,10 +6,7 @@ def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
         for j in range(n - 1, -1, -1):
-            if arr1[i] & 1 << j or arr2[i] & 1 << j:
-                maze_string += "#"
-            else:
-                maze_string += " "
+            maze_string += "#" if arr1[i] & 1 << j or arr2[i] & 1 << j else " "
         answer.append(maze_string)
         maze_string = ""
     return answer
