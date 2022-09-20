3355534a4c2b87662ecc15bb3fe6207cdcd6b405
dokkyunYU <ydgar001@naver.com>
2022-09-20 21:05:10 +0900

    Feat: 17681번 문제 해결

--- NBA_deokgyun/09/week_3rd/17681.py
+++ NBA_deokgyun/09/week_3rd/17681.py
@@ -1,1 +1,15 @@
 # [Programmers] 17681. [1차] 비밀지도
+
+
+def solution(n, arr1, arr2):
+    maze_string = ""
+    answer = []
+    for i in range(n):
+        for j in range(n - 1, -1, -1):
+            if arr1[i] & 1 << j or arr2[i] & 1 << j:
+                maze_string += "#"
+            else:
+                maze_string += " "
+        answer.append(maze_string)
+        maze_string = ""
+    return answer
