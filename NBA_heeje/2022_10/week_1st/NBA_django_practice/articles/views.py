6159cc99ed9151761cbcf999334181c9cc3de15b
HeeJeYang <ghjadmb21@gmail.com>
2022-10-02 14:16:30 +0900

    Feat: 10월 1주차 Django 복습 완료

---
+++ NBA_heeje/2022_10/week_1st/NBA_django_practice/articles/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_safe, require_POST, require_http_methods
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+@require_safe
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'articles/create.html', context)
+
+@require_safe
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'articles/detail.html', context)
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context= {
+        'form': form,
+        'article': article,
+    }
+
+    return render(request, 'articles/update.html', context)
+
+@require_POST
+def delete(request, pk):
+    if request.user.is_authenticated:
+        article = Article.objects.get(pk=pk)
+        article.delete()
+
+    return redirect('articles:index')
