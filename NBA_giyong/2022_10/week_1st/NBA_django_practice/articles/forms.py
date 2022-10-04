7ac3f1be75c9de91a2ce4543e041861a41b496a0
KI-DG <kanfs2022@gmail.com>
2022-10-04 10:56:38 +0900

    Feat: NBA_django_practice

---
+++ NBA_giyong/2022_10/week_1st/NBA_django_practice/articles/forms.py
@@ -0,0 +1,31 @@
+from dataclasses import field
+from pyexpat import model
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label = 'Title',
+        widget = forms.TextInput(
+            attrs={
+                'placeholder': '제목을 입력해주세요',
+            }
+        )
+    )
+    content = forms.CharField(
+        label = 'Content',
+        widget = forms.Textarea(
+            attrs={
+                'placeholder': '내용을 입력해주세요',
+                # 'rows': 5,
+                # 'cols': 50,
+            }
+        ),
+        error_messages = {
+            'requeired': '내용은 필수로 입력해주세요',
+        }
+    )
+
+    class Meta:
+        model = Article
+        fields = '__all__'
