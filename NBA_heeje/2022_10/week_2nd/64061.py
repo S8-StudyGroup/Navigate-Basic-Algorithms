42bdbcd3325ce594f5325411bda71cfa6d3c5cb4
HeeJeYang <ghjadmb21@gmail.com>
2022-10-07 22:15:57 +0900

    Feat: 64061번 문제 해결

--- NBA_heeje/2022_10/week_2nd/64061.py
+++ NBA_heeje/2022_10/week_2nd/64061.py
@@ -1,1 +1,18 @@
 # [Programmers] 64061. 크레인 인형뽑기 게임
+
+
+def solution(board, moves):
+    answer = 0
+    stack = []
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move - 1] != 0:
+                pick_up = board[i][move - 1]
+                board[i][move - 1] = 0
+                if stack and stack[-1] == pick_up:
+                    stack.pop()
+                    answer += 2
+                else:
+                    stack.append(pick_up)
+                break
+    return answer
