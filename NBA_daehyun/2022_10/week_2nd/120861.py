060dab1cd97dc1dbac3176d278f13258da7d7fd3
HeeJeYang <ghjadmb21@gmail.com>
2022-10-07 21:07:55 +0900

    Feat: 10월 2주차 문제 업로드

--- NBA_daehyun/2022_10/week_2nd/120861.py
+++ NBA_daehyun/2022_10/week_2nd/120861.py
@@ -1,29 +1,31 @@
-#캐릭터의 좌표
+# [Programmers] 120861. 캐릭터의 좌표
+
+
 def solution(keyinput, board):
     start = [0, 0]
     garo = board[0] // 2
     sero = board[1] // 2
     for i in keyinput:
-        if i =='left':
+        if i == 'left':
             a = start[0] + (-1)
             b = start[1] + 0
             if -garo <= a <= garo and -sero <= b <= sero:
                 start[0], start[1] = a, b
-        if i =='right':
+        if i == 'right':
             a = start[0] + 1
             b = start[1] + 0
             if -garo <= a <= garo and -sero <= b <= sero:
                 start[0], start[1] = a, b
-        if i =='up':
+        if i == 'up':
             a = start[0] + 0
             b = start[1] + 1
             if -garo <= a <= garo and -sero <= b <= sero:
                 start[0], start[1] = a, b
-        if i =='down':
+        if i == 'down':
             a = start[0] + 0
             b = start[1] + -1
             if -garo <= a <= garo and -sero <= b <= sero:
                 start[0], start[1] = a, b
-    
+
     answer = start
     return answer
