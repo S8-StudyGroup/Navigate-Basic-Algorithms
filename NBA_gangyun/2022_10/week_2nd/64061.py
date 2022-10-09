98948fc0ee5add117c09def368070cf5297f892f
이강윤 <lky61010@naver.com>
2022-10-09 16:15:05 +0900

    Feat: 64061번 문제 해결

--- NBA_gangyun/2022_10/week_2nd/64061.py
+++ NBA_gangyun/2022_10/week_2nd/64061.py
@@ -1,1 +1,26 @@
 # [Programmers] 64061. 크레인 인형뽑기 게임
+
+def solution(board, moves):
+    size = len(board)
+    basket = [0]
+    disappeared = 0
+    for j in moves:
+        pick = 0
+        for i in range(size):
+            if board[i][j - 1] != 0:
+                pick = board[i][j - 1]
+                board[i][j - 1] = 0
+                break
+
+        if pick != 0:
+            if pick != basket[-1]:
+                basket.append(pick)
+            else:
+                basket.pop()
+                disappeared += 2
+
+    answer = disappeared
+    return answer
+
+
+# print(solution([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1, 5, 3, 5, 1, 2, 1, 4]))
