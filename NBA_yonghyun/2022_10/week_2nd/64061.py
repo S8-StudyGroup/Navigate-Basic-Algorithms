461fa58e864653044d71d4f3030ae314d3496378
yonghyunc <chyh9218@gmail.com>
2022-10-11 18:05:50 +0900

    Feat: 64061번 문제 해결

--- NBA_yonghyun/2022_10/week_2nd/64061.py
+++ NBA_yonghyun/2022_10/week_2nd/64061.py
@@ -1,1 +1,29 @@
 # [Programmers] 64061. 크레인 인형뽑기 게임
+
+
+
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+
+
+def solution(board, moves):
+    n = len(board)
+    basket = [0]
+    answer = 0
+
+    for turn in moves:
+        for i in range(n):
+            if board[i][turn - 1] != 0:
+                # print(board[i][turn - 1])
+                basket.append(board[i][turn - 1])
+                board[i][turn - 1] = 0
+                # print(turn, basket)
+                if basket[-1] == basket[-2]:
+                    basket.pop()
+                    basket.pop()
+                    answer += 2
+                break
+    return answer
+
+
+print(solution(board, moves))
