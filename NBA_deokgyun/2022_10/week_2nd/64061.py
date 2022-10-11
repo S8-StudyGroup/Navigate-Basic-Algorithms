2ff70a36709275d62a56ee4f74c3db2dece77be7
dokkyunYU <ydgar001@naver.com>
2022-10-11 21:02:16 +0900

    Feat: 64061번 문제 해결

--- NBA_deokgyun/2022_10/week_2nd/64061.py
+++ NBA_deokgyun/2022_10/week_2nd/64061.py
@@ -1,1 +1,18 @@
 # [Programmers] 64061. 크레인 인형뽑기 게임
+
+
+def solution(board, moves):
+    answer = []
+    answer_cnt = 0
+    b_size = len(board)
+    for k in moves:
+        for i in range(b_size):
+            if board[i][k - 1]:
+                if answer == [] or answer[-1] != board[i][k - 1]:
+                    answer.append(board[i][k - 1])
+                elif answer[-1] == board[i][k - 1]:
+                    answer.pop()
+                    answer_cnt += 2
+                board[i][k - 1] = 0
+                break
+    return answer_cnt
