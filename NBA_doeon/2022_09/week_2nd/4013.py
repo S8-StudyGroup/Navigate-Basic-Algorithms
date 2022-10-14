cf531e2450aef256f19ee94184010340286b87f1
eondo <vov06144@naver.com>
2022-10-14 10:11:09 +0900

    Feat: 4013번 문제 해결

--- NBA_doeon/2022_09/week_2nd/4013.py
+++ NBA_doeon/2022_09/week_2nd/4013.py
@@ -1,45 +1,57 @@
 # 4013. 특이한 자석
 
+
+def find_target(target_m, dir):
+    visited[target_m] = True
+    left = target_m - 1
+    right = target_m + 1
+
+    if 1 <= left <= 4 and not visited[left]:
+        if mag[target_m][6] != mag[left][2]:  # 타겟의 왼쪽
+            will_be_turned.append((left, -dir))
+            find_target(left, -dir)
+    if 1 <= right <= 4 and not visited[right]:
+        if mag[target_m][2] != mag[right][6]:  # 타겟의 오른쪽
+            will_be_turned.append((right, -dir))
+            find_target(right, -dir)
+
+
 t = int(input())
-k = int(input())
-
 for tc in range(1, t + 1):
-    arr = [list(map(int, input().split())) for _ in range(4)]
-    turn = [list(map(int, input().split())) for _ in range(k)]
+    k = int(input())
 
-    print(arr)
-    print(turn)
+    mag = [list(map(int, input().split())) for _ in range(4)]
+    mag = [0] + mag
+    plays = [list(map(int, input().split())) for _ in range(k)]
+    score = [0, (0, 1), (0, 2), (0, 4), (0, 8)]
 
-    # 일단 제일 먼저 뭘 해야 하냐면 뭘 돌릴 건지 정해야지 그건 turn 리스트에 있음
+    # print(mag)
 
-    for i in range(len(turn)):
-        gear = turn[i][0] # 얘가 돌릴 주체 기어 번호가 나옴
-        direction = turn[i][1] # 돌릴 방향 나옴 1 또는 -1
+    for play in plays:
+        start_m = play[0]
+        d = play[1]
 
-        # 돌릴 애 정했으면 이제 뭘 봐야해? 돌릴 애의 2,6번째 인덱스랑 그 옆의 톱니바퀴의 6,2 인덱스
-        # 체크했을 때, 둘이 다르면, direction과 반대로 해당 arr를 돌린다
+        will_be_turned = [(start_m, d)]
+        visited = [True] + [False] * 4
+        # print(visited)
+        find_target(start_m, d)
 
-        # 해당 기어의 2번째랑 그 오른쪽 기어의 6번째가 다르면?
-        if 0 <= gear - 1 + 1 < 8 and arr[gear - 1][2] != arr[gear - 1 + 1][6]:
-            # 해당 기어도 돌리고, 오른쪽 애는 반대로 돌리자
-            if direction == 1:
-                arr[gear - 1].insert(0, arr[gear - 1].pop())
-                arr[gear - 1 + 1].insert(7, arr[gear - 1 + 1].pop(0))
+        # 그럼 이제 나온 wbt으로 자석들 리스트 pop해서 한칸씩 이동해주자
+        for turn in will_be_turned:
+            mag_num, turn_dir = turn
 
-            elif direction == -1:
-                arr[gear - 1].insert(7, arr[gear - 1].pop(0))
-                arr[gear - 1 + 1].insert(0, arr[gear - 1 + 1].pop())
+            if turn_dir == 1:
+                last = mag[mag_num].pop()
+                mag[mag_num] = [last] + mag[mag_num]
+            elif turn_dir == -1:
+                first = mag[mag_num].pop(0)
+                mag[mag_num] = mag[mag_num] + [first]
 
+    # 이제 돌리기 게임 끝! 점수 합산할 시간
+    score_cnt = 0
+    # print(len(mag))
+    for idx in range(1, len(mag)):
+        position = mag[idx][0]
+        score_cnt += score[idx][position]
 
-        # 해당 기어의 6번째랑 그 왼쪽의 기어의 2번째가 다르면?
-        elif 0 <= gear - 1 - 1 < 8 and arr[gear - 1][6] != arr[gear - 1 - 1][2]:
-            # 해당 기어도 돌리고, 오른쪽 애는 반대로 돌리자
-            if direction == 1:
-                arr[gear - 1].insert(0, arr[gear - 1].pop())
-                arr[gear - 1 + 1].insert(7, arr[gear - 1 + 1].pop(0))
-
-            elif direction == -1:
-                arr[gear - 1].insert(7, arr[gear - 1].pop(0))
-                arr[gear - 1 + 1].insert(0, arr[gear - 1 + 1].pop())
-
-    print(arr)
+    print(f'#{tc} {score_cnt}')
