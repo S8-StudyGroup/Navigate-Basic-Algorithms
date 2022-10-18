75acfce601ef75e165d60d96ffa4b416a2b6cef6
dokkyunYU <ydgar001@naver.com>
2022-10-18 17:02:55 +0900

    Feat: 67256번 문제 해결

--- NBA_deokgyun/2022_10/week_3rd/67256.py
+++ NBA_deokgyun/2022_10/week_3rd/67256.py
@@ -1,1 +1,33 @@
 # [Programmers] 67256. 키패드 누르기
+
+def solution(numbers, hand):
+    left = 9
+    right = 11
+    answer = ''
+    for i in numbers:
+        i = i - 1
+        if i == -1:
+            i = 10
+        if i % 3 == 1:
+            l2 = abs(left // 3 - i // 3) + abs(left % 3 - i % 3)
+            r2 = abs(right // 3 - i // 3) + abs(right % 3 - i % 3)
+            if l2 == r2:
+                if hand == "left":
+                    left = i
+                    answer += "L"
+                else:
+                    right = i
+                    answer += "R"
+            elif l2 > r2:
+                right = i
+                answer += "R"
+            else:
+                left = i
+                answer += "L"
+        elif i % 3 == 0:
+            left = i
+            answer += "L"
+        else:
+            right = i
+            answer += "R"
+    return answer
