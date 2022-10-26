b6c05799f0fff5ecced573d04e5a0ca64b3304b3
ssg-js <likemath1997@gmail.com>
2022-10-26 21:24:08 +0900

    Feat: 14510번 문제 미해결

--- NBA_jinseong/2022_10/week_4th/14510.py
+++ NBA_jinseong/2022_10/week_4th/14510.py
@@ -1,1 +1,24 @@
 # [SWEA] 14510. 나무 높이
+# 미해결
+
+
+for t in range(1, int(input()) + 1):
+    N = int(input())
+    heights = list(map(int, input().split()))
+
+    max_height = max(heights)
+
+    time_grow = 0
+
+    for height in heights:
+        time_grow += max_height - height
+
+    result = 0
+    if time_grow <= 2:
+        result = time_grow
+    elif time_grow > 2:
+        result += (time_grow // 3) * 2
+        rest_time = time_grow % 3
+        result += rest_time
+
+    print(f'#{t} {result}')
