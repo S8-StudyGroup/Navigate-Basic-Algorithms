860f312ec6d3ccea43c5db05fa857b2baaf294c9
happyd918 <0happydeath0@naver.com>
2022-11-07 23:28:51 +0900

    Feat: 15686번 문제 해결

--- NBA_taehak/2022_11/week_2nd/15686.py
+++ NBA_taehak/2022_11/week_2nd/15686.py
@@ -1,1 +1,28 @@
-# [BOJ] 15686. 치킨 배달
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+city = [list(map(int, input().split())) for _ in range(n)]
+result = 999999
+houses = []
+bbqs = []
+
+for r in range(n):
+    for c in range(n):
+        if city[r][c] == 1:
+            houses.append([r, c])
+        elif city[r][c] == 2:
+            bbqs.append([r, c])
+
+for bbq_case in combinations(bbqs, m):
+    memo = 0
+    for hr, hc in houses:
+        bbq_len = 999999
+        for br, bc in bbq_case:
+            bbq_len = min(bbq_len, abs(hr - br) + abs(hc - bc))
+        memo += bbq_len
+    result = min(result, memo)
+
+print(result)
