1b3d94bee38ba2fe84d8274568b5f178bfe45e96
0happydeath0 <0happydeath0@naver.com>
2022-11-08 17:47:04 +0900

    Feat: 15686번 문제 주석 추가

--- NBA_taehak/2022_11/week_2nd/15686.py
+++ NBA_taehak/2022_11/week_2nd/15686.py
@@ -5,10 +5,9 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 city = [list(map(int, input().split())) for _ in range(n)]
-result = 999999
+
 houses = []
 bbqs = []
-
 for r in range(n):
     for c in range(n):
         if city[r][c] == 1:
@@ -16,13 +15,15 @@ for r in range(n):
         elif city[r][c] == 2:
             bbqs.append([r, c])
 
-for bbq_case in combinations(bbqs, m):
-    memo = 0
-    for hr, hc in houses:
-        bbq_len = 999999
-        for br, bc in bbq_case:
-            bbq_len = min(bbq_len, abs(hr - br) + abs(hc - bc))
-        memo += bbq_len
-    result = min(result, memo)
+
+result = 999999
+for bbq_case in combinations(bbqs, m):                              # m개의 치킨집을 고르는 경우의수 마다
+    case_memo = 0                                                   # case_memo : 이번 경우의 치킨거리 합
+    for hr, hc in houses:                                           # 집마다
+        bbq_len = 999999                                            # 치킨거리(최소거리)를 구한다
+        for br, bc in bbq_case:                                     # 치킨집마다
+            bbq_len = min(bbq_len, abs(hr - br) + abs(hc - bc))     # 거리를 구해서 치킨거리를 구한다.
+        case_memo += bbq_len                                        # 해당하는 집의 치킨거리를 case_memo에 더해준다
+    result = min(result, case_memo)                                 # 경우의 수마다 치킨거리의 합을 갱신(최소값으로)
 
 print(result)
