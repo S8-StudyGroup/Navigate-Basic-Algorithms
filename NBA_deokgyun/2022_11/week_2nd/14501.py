c2ff970e68081c8d3f0e124fe3f0108b51bb6a75
dokkyunYU <ydgar001@naver.com>
2022-11-08 17:56:31 +0900

    Feat: 14501번, 15686번 문제 해결

--- NBA_deokgyun/2022_11/week_2nd/14501.py
+++ NBA_deokgyun/2022_11/week_2nd/14501.py
@@ -1,1 +1,20 @@
 # [BOJ] 14501. 퇴사
+
+def dfs(nowlocation=0, totalsum=0):
+    global maxtotalsum, n
+    if nowlocation > n:
+        return
+    elif nowlocation == n:
+        if totalsum > maxtotalsum:
+            maxtotalsum = totalsum
+        return
+    dfs(nowlocation + dayslist[nowlocation][0], totalsum + dayslist[nowlocation][1])
+    dfs(nowlocation + 1, totalsum)
+    
+
+
+n = int(input())
+dayslist = [tuple(map(int, input().split())) for _ in range(n)]
+maxtotalsum = 0
+dfs()
+print(maxtotalsum)
--- NBA_deokgyun/2022_11/week_2nd/15686.py
+++ NBA_deokgyun/2022_11/week_2nd/15686.py
@@ -1,1 +1,34 @@
 # [BOJ] 15686. 치킨 배달
+
+import itertools
+
+n, m = map(int, input().split())
+city = [list(map(int, input().split())) for _ in range(n)]
+house = []
+chicken = []
+for i in range(n):
+    for j in range(n):
+        if city[i][j] == 1:
+            house.append((i,j))
+        elif city[i][j] == 2:
+            chicken.append((i,j))
+houselen = len(house)
+chickenlen = len(chicken)
+house_chicken = [[] for _ in range(houselen)]
+eachhouse = 0
+for h in house:
+    for c in chicken:
+        house_chicken[eachhouse].append(abs(h[0] - c[0]) + abs(h[1] - c[1]))
+    eachhouse += 1
+minchickendist = 10**6
+for open in itertools.combinations(range(chickenlen), m):
+    chickendist = 0
+    for i in range(houselen):
+        mindist = 10**6
+        for j in open:
+            if house_chicken[i][j] < mindist:
+                mindist = house_chicken[i][j]
+        chickendist += mindist
+    if chickendist < minchickendist:
+        minchickendist = chickendist
+print(minchickendist)
