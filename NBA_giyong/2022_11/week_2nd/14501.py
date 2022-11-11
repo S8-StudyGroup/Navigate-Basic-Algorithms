03530002da4c615bfec43545f0e61a0f75bc21c0
KI-DG <kanfs2022@gmail.com>
2022-11-11 22:25:33 +0900

    feat: 14501번 문제 해결

--- NBA_giyong/2022_11/week_2nd/14501.py
+++ NBA_giyong/2022_11/week_2nd/14501.py
@@ -1,1 +1,18 @@
 # [BOJ] 14501. 퇴사
+n = int(input())
+arr = [list(map(int, (input().split()))) for _ in range(n)]
+
+answer = [0] * (n + 1)
+
+for i in range(n - 1, -1, -1):
+    if i + arr[i][0] > n:
+        answer[i] = answer[i + 1]
+
+    else:
+        result = answer[i + arr[i][0]] + arr[i][1]
+        if result > answer[i + 1]:
+            answer[i] = result
+        else:
+            answer[i] = answer[i + 1]
+
+print(max(answer))
