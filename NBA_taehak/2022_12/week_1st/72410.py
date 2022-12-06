e8ecaf81a0e775d95ee64338f051e028b0e873e3
0happydeath0 <0happydeath0@naver.com>
2022-12-07 01:20:50 +0900

    Feat: 72410번 문제 해결

--- NBA_taehak/2022_12/week_1st/72410.py
+++ NBA_taehak/2022_12/week_1st/72410.py
@@ -1,1 +1,34 @@
 # [Programmers] 72410. 신규 아이디 추천
+
+def solution(new_id):
+    # 1, 2, 3
+    condition = {'-', '_'}
+    memo = '.'
+    for char in new_id:
+        if char.isalpha():
+            memo += char.lower()
+        elif char.isdecimal():
+            memo += char
+        elif char in condition:
+            memo += char
+        elif char == '.' and memo[-1] != '.':
+            memo += char
+    
+    # 4
+    memo = memo.strip('.')
+
+    # 5
+    if memo == '':
+        memo = 'a'
+
+    # 6
+    if len(memo) >= 16:
+        memo = memo[:15]
+    memo = memo.rstrip('.')
+
+    # 7
+    while len(memo) < 3:
+        memo += memo[-1]        
+
+    return memo
+
