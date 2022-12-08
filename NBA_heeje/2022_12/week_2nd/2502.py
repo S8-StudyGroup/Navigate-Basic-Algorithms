433306772065de549e799e0c33eda892dedef2bc
HeeJeYang <ghjadmb21@gmail.com>
2022-12-08 10:15:31 +0900

    Feat: 2502번 문제 해결

--- NBA_heeje/2022_12/week_2nd/2502.py
+++ NBA_heeje/2022_12/week_2nd/2502.py
@@ -1,1 +1,19 @@
 # [BOJ] 2502. 떡 먹는 호랑이
+
+
+def chk():
+    for i in range(1, 100000):
+        for j in range(i, 100000):
+            temp_list = [i, j]
+            for _ in range(D - 2):
+                temp_list.append(temp_list[-2] + temp_list[-1])
+            if temp_list[-1] == K:
+                return [i, j]
+            elif temp_list[-1] > K:
+                break
+
+
+D, K = map(int, input().split())
+
+answer = chk()
+print(*answer, sep="\n")
