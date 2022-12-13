43c2289b28211755a3eb38452cbb7739a0345551
dokkyunYU <ydgar001@naver.com>
2022-12-13 20:15:39 +0900

    Feat : 2502번, 42889번 문제 해결

--- NBA_deokgyun/2022_12/week_2nd/2502.py
+++ NBA_deokgyun/2022_12/week_2nd/2502.py
@@ -1,1 +1,31 @@
 # [BOJ] 2502. 떡 먹는 호랑이
+
+# 우선 피보나치 수열
+# 그 후 정수해를 구하기
+
+# 1. A
+# 2. B
+# 3. A + B    1 1
+# 4. A + 2B   1 2
+# 5. 2A + 3B  2 3
+# 6. 3A + 5B  3 5
+# 7. 5A + 8B  5 8
+# A : 1 1 2 3 5 피보나치 수열의 n-2번째 수
+# B : 1 2 3 5 8 피보나치 수열의 n-1번째 수
+
+
+nums = [0]
+day, ddeok = map(int, input().split())
+for i in range(1, day):
+    if i == 1 or i == 2:
+        nums.append(1)
+        continue
+    nums.append(nums[-2] + nums[-1])
+Anum, Bnum = nums[day - 2 : day]
+# Anum * A + Bnum * B = ddeok
+for B in range(1, ddeok // Bnum + 1):
+    A = (ddeok - Bnum * B) // Anum
+    if (ddeok - Bnum * B) % Anum or A > B:
+        continue
+    print(A, B, sep="\n")
+    break
--- NBA_deokgyun/2022_12/week_2nd/42889.py
+++ NBA_deokgyun/2022_12/week_2nd/42889.py
@@ -1,1 +1,23 @@
 # [Programmers] 42889. 실패율
+
+
+def solution(N, stages):
+    numlist = [0] * (N + 2)
+    sumlist = [0] * (N + 2)
+    numsum = 0
+    for i in stages:
+        numlist[i] += 1
+        sumlist[i] += 1
+    for i in range(N + 1, 0, -1):
+        sumlist[i] = sumlist[i] + numsum
+        numsum = sumlist[i]
+    failslist = []
+    answer = []
+    for i in range(1, N + 1):
+        if sumlist[i] != 0:
+            failslist.append((i, numlist[i] / sumlist[i]))
+        else:
+            failslist.append((i, 0))
+    for i in sorted(failslist, key=lambda x: (-x[1], x[0])):
+        answer.append(i[0])
+    return answer
