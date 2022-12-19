8aa58ceb2f2e6a314b29e91bb9ecf405a23c2962
happyd918 <0happydeath0@naver.com>
2022-12-19 10:31:35 +0900

    Feat: 1389번 문제 해결

--- NBA_taehak/2022_12/week_3rd/1389.py
+++ NBA_taehak/2022_12/week_3rd/1389.py
@@ -1,1 +1,24 @@
 # [BOJ] 1389. 케빈 베이컨의 6단계 법칙
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+node_cnt, edge_cnt = map(int, input().split())
+
+graph = [[] for _ in range(node_cnt + 1)]
+for _ in range(edge_cnt):
+    a, b = map(int ,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+def bfs(start):
+    kevin = [0] * (node_cnt + 1)
+    visited = set()
+    que = deque()
+    
+    visited.add(start)
+    que.append(start)
+
+    while que:
+        node = que.popleft()
