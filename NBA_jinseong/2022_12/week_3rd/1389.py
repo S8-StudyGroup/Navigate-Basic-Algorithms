d6c2b7e4d7af5f12ecf0a7cf183c112dd17fc9e0
ssg-js <likemath1997@gmail.com>
2022-12-21 03:42:51 +0900

    Feat: 1389번 문제 해결

--- NBA_jinseong/2022_12/week_3rd/1389.py
+++ NBA_jinseong/2022_12/week_3rd/1389.py
@@ -1,1 +1,39 @@
 # [BOJ] 1389. 케빈 베이컨의 6단계 법칙
+
+def bfs(num):
+    visited = [0] * (N + 1)
+    queue = [num]
+    visited[num] = 1
+
+    while queue:
+        now = queue.pop(0)
+        for next in rel[now]:
+            if visited[next] == 0:
+                visited[next] = visited[now] + 1
+                queue.append(next)
+    return sum(visited) - N
+
+
+N, M = map(int, input().split())
+
+rel = [[] for _ in range(N + 1)]                # 친구 관계 저장
+kb_num = [0] * (N + 1)                          # 해당 유저의 케빈베이컨 수 저장
+
+for i in range(M):
+    a, b = map(int, input().split())
+    rel[a].append(b)
+    rel[b].append(a)
+
+for i in range(1, N + 1):
+    kb_num[i] = bfs(i)
+
+kb_min = min(kb_num[1:])
+answer = 0
+
+for i in range(1, N + 1):
+    if kb_num[i] == kb_min:
+        answer = i
+        break
+
+print(answer)
+
