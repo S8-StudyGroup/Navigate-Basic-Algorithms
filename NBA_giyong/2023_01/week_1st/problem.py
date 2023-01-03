ff4410871dc9016218849666503ccb9ec508b86d
KI-DG <kanfs2022@gmail.com>
2023-01-03 22:10:54 +0900

    Feat: 1260번 문제해결

--- NBA_giyong/2023_01/week_1st/problem.py
+++ NBA_giyong/2023_01/week_1st/problem.py
@@ -1,1 +1,39 @@
-# 문제 이름
+# 문제 이름 1260번 DFS와 BFS
+
+from collections import deque
+
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visit_list[v] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
+        for i in range(1, n + 1):
+            if visit_list[i] == 0 and graph[v][i] == 1:
+                q.append(i)
+                visit_list[i] = 1
+
+
+def dfs(v):
+    visit_list2[v] = 1
+    print(v, end=' ')
+    for i in range(1, n + 1):
+        if visit_list2[i] == 0 and graph[v][i] == 1:
+            dfs(i)
+
+
+n, m, v = map(int, input().split())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+visit_list = [0] * (n + 1)
+visit_list2 = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = graph[b][a] = 1
+
+dfs(v)
+print()
+bfs(v)
