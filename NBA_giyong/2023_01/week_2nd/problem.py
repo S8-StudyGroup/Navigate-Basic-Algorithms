2483cd426473d5c4b8b7d0c8050b81ee25cf0d9d
KI-DG <kanfs2022@gmail.com>
2023-01-16 23:31:21 +0900

    feat: 11866번 문제 해결

--- NBA_giyong/2023_01/week_2nd/problem.py
+++ NBA_giyong/2023_01/week_2nd/problem.py
@@ -1,1 +1,21 @@
-# [BOJ] 1234. 문제 이름
+# [BOJ] 11866. 요세푸스문제
+from collections import deque
+
+n, k = map(int, input().split())
+s = deque([])
+
+for i in range(1, n + 1):
+    s.append(i)
+
+print('<', end='')
+
+while s:
+    for i in range(k - 1):
+        s.append(s[0])
+        s.popleft()
+    print(s.popleft(), end='')
+
+    if s:
+        print(', ', end='')
+
+print('>')
